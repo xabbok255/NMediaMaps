12b41c17411625bb68260aa4dbf3d6c468891831
xabbok255 <xabbok255@ya.ru>
2023-06-09 11:43:53 +0300

    added bottomNav.setupWithNavController fixed fragment names in navigation_graph val MarkerManager is moved to the MapsFragment class as a property

--- app/src/main/java/com/xabbok/nmediamaps/MainActivity.kt
+++ app/src/main/java/com/xabbok/nmediamaps/MainActivity.kt
@@ -1,52 +1,23 @@
 package com.xabbok.nmediamaps
 
-import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.xabbok.nmediamaps.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
     private val binding: ActivityMainBinding by viewBinding(ActivityMainBinding::bind)
     private val navHostController by lazy {
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
         val navController = navHostFragment.navController
         navController
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onStart() {
+        super.onStart()
 
-        binding.bottomNav.setOnItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.mainMap -> {
-
-                    navHostController.navigate(R.id.action_global_mapsFragmentNav)
-                    true
-                }
-
-                R.id.objectList -> {
-                    navHostController.navigate(R.id.action_global_objectsFragmentNav)
-                    true
-                }
-
-                R.id.settings -> {
-
-                    true
-                }
-
-                else -> {
-                    false
-                }
-            }
-        }
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-
-
+        binding.bottomNav.setupWithNavController(navHostController)
     }
 }
--- app/src/main/java/com/xabbok/nmediamaps/MapsFragment.kt
+++ app/src/main/java/com/xabbok/nmediamaps/MapsFragment.kt
@@ -10,7 +10,9 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.content.ContextCompat.getDrawable
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -24,10 +26,13 @@ import com.google.maps.android.ktx.model.cameraPosition
 import com.google.maps.android.ktx.utils.collection.addMarker
 import com.xabbok.nmediamaps.databinding.FragmentMapsBinding
 import com.xabbok.nmediamaps.extensions.icon
+import kotlinx.coroutines.launch
 
 class MapsFragment : Fragment(R.layout.fragment_maps) {
     private lateinit var googleMap: GoogleMap
+    private val marketManager: MarkerManager by lazy { MarkerManager(googleMap) }
     private val binding: FragmentMapsBinding by viewBinding(FragmentMapsBinding::bind)
+    private val mapFragment: SupportMapFragment by lazy { childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment }
 
     @SuppressLint("MissingPermission")
     val requestPermissionsLauncher =
@@ -43,92 +48,101 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
         }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val mapFragment =
-            childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(state = Lifecycle.State.RESUMED) {
+                googleMap = mapFragment.awaitMap()
 
-        lifecycleScope.launchWhenCreated {
-            googleMap = mapFragment.awaitMap()
-
-            when {
-                ContextCompat.checkSelfPermission(
-                    requireContext(),
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                ) == PackageManager.PERMISSION_GRANTED -> {
-                    googleMap.apply {
-                        isMyLocationEnabled = true
-                        uiSettings.isMyLocationButtonEnabled = true
-                    }
-
-                    LocationServices.getFusedLocationProviderClient(requireContext())
-                        .lastLocation.addOnSuccessListener {
-                            Toast.makeText(
-                                requireContext(),
-                                "Last location: $it",
-                                Toast.LENGTH_LONG
-                            )
-                                .show()
+                when {
+                    ContextCompat.checkSelfPermission(
+                        requireContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED -> {
+                        googleMap.apply {
+                            isMyLocationEnabled = true
+                            uiSettings.isMyLocationButtonEnabled = true
                         }
-                }
 
-                shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
-                    Toast.makeText(requireContext(), "need GPS !!!", Toast.LENGTH_LONG)
-                        .show()
-                }
-
-                else -> requestPermissionsLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
-            }
-
-            googleMap.apply {
-                isTrafficEnabled = true
-                isBuildingsEnabled = true
-                uiSettings.apply {
-                    isZoomControlsEnabled = true
-                    setAllGesturesEnabled(true)
-                }
-            }
-
-            val target = LatLng(55.751999, 37.617734)
-            googleMap.awaitAnimateCamera(
-                CameraUpdateFactory.newCameraPosition(
-                    cameraPosition {
-                        target(target)
-                        zoom(15F)
+                        LocationServices.getFusedLocationProviderClient(requireContext())
+                            .lastLocation.addOnSuccessListener {
+                                Toast.makeText(
+                                    requireContext(),
+                                    "Last location: $it",
+                                    Toast.LENGTH_LONG
+                                )
+                                    .show()
+                            }
                     }
+
+                    shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION) -> {
+                        Toast.makeText(requireContext(), "need GPS !!!", Toast.LENGTH_LONG)
+                            .show()
+                    }
+
+                    else -> requestPermissionsLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+                }
+
+                googleMap.apply {
+                    isTrafficEnabled = true
+                    isBuildingsEnabled = true
+                    uiSettings.apply {
+                        isZoomControlsEnabled = true
+                        setAllGesturesEnabled(true)
+                    }
+                }
+
+                val target = LatLng(55.751999, 37.617734)
+                googleMap.awaitAnimateCamera(
+                    CameraUpdateFactory.newCameraPosition(
+                        cameraPosition {
+                            target(target)
+                            zoom(15F)
+                        }
+                    )
                 )
-            )
+                
+                val collection: MarkerManager.Collection = marketManager.newCollection().apply {
+                    addMarker {
+                        position(target)
 
-            val marketManager = MarkerManager(googleMap)
-            val collection: MarkerManager.Collection = marketManager.newCollection().apply {
-                addMarker {
-                    position(target)
-
-                    icon(
-                        requireNotNull(
-                            getDrawable(
-                                requireContext(),
-                                R.drawable.baseline_question_mark_24
+                        icon(
+                            requireNotNull(
+                                getDrawable(
+                                    requireContext(),
+                                    R.drawable.baseline_question_mark_24
+                                )
                             )
                         )
-                    )
-                    title("12345")
-                }.apply {
-                    tag = "Data 123"
+                        title("12345")
+                    }.apply {
+                        tag = "Data 123"
+                    }
+                }
+
+                collection.setOnMarkerClickListener { marker ->
+                    Toast.makeText(
+                        requireContext(),
+                        (marker.tag as String),
+                        Toast.LENGTH_LONG
+                    ).show()
+                    true
                 }
             }
-
-            collection.setOnMarkerClickListener { marker ->
-                Toast.makeText(
-                    requireContext(),
-                    (marker.tag as String),
-                    Toast.LENGTH_LONG
-                ).show()
-                true
-            }
         }
-//
-//        googleMap.setOnMapClickListener {
-//
-//        }
+    }
 
+    override fun onResume() {
+        super.onResume()
+        mapFragment.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapFragment.onPause()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        mapFragment.onDestroyView()
     }
 }
